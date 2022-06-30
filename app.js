f8ab9bed998bc53d01abb2c80882ca1590ba6e07
klajdikolaj <klajdi.kolaj@siemplify.co>
2022-06-30 08:12:56 +0200

    adding layouts and ejs

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const http = require('http');
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const adminRoutes = require('./routes/admin');
-const bodyParser = require('body-parser')
-const shopRoutes = require('./routes/shop')
-const path = require("path");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin', adminRoutes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next) =>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
