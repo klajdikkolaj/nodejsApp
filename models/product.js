bb14a7a9a10cca4b4b41fec634a32e04ef1cdc22
klajdikolaj <klajdi.kolaj@siemplify.co>
2022-06-30 11:01:28 +0200

    refactorings after model controllers

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const fs = require('fs');
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+//helper function
+const getProductsFromFile = cb => {
+
+    fs.readFile(p,(err,fileContent) =>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
+
+module.exports = class Product{
+    constructor(t) {
+        this.title = t;
+    }
+
+    save(){
+        getProductsFromFile(products =>{
+            products.push(this);
+            fs.writeFile( p, JSON.stringify(products), (err) => {
+                console.log(err)
+            });
+        })
+    }
+
+    static fetchAll(cb){
+      getProductsFromFile(cb)
+    }
+}
